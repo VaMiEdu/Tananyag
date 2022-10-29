6d3cdd1f9d28fd3617c08682d4e54d809846d10e
Mihaly Varga <mihaly.varga@elpro.com>
2022-10-29 17:58:23 +0200

    Első órai anyag

---
+++ elso_ora/HomeWorks_Upload/HomeWorks/HomeWorks/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace HomeWorks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BevasrloLista
+
+            //string ElsoTetel;
+            //int ElsoTetelMennyisege;
+
+            //string MasodikTetel;
+            //int MasodikTetelMennyisege;
+
+            //Console.WriteLine("Adjuk meg az elso tetelt!");
+            //ElsoTetel = Console.ReadLine();
+
+            //Console.WriteLine("Adjuk meg az elso tetelhez tartozó mennyiséget!");
+            //ElsoTetelMennyisege = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine(ElsoTetel);
+            //Console.WriteLine(ElsoTetel);
+
+            //----------------------------------------------------------------
+
+
+            //Szamok bekerese
+
+            //Console.WriteLine("Adjon meg egy szamot!");
+
+            //int szam = Convert.ToInt32(Console.ReadLine());
+
+            //if (szam % 3 == 0)
+            //{
+            //    Console.WriteLine("oszthato harommal.");
+            //}
+
+            //-----------------------------------------------------
+
+            // víz gőz jég 
+
+            //int szam = Convert.ToInt32(Console.ReadLine());
+
+            //if (szam >= 100)
+            //{
+            //    Console.WriteLine("A homerseklet alapjan gaz halmazallapotu.");
+            //}
+            //else if (szam <= 0)
+            //{
+            //    Console.WriteLine("A homerseklet alapjan jeg halmazallapotu.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("A homerseklet alapjan folyekony halmazallapotu.");
+            //}
+
+            //-----------------------------------------------------
+
+            //Paros szamok osszege
+
+            //int sum = 0;
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine($"Oszthato 5-tel: {i}");
+            //        sum += i;
+            //    }
+            //}
+
+            //Console.WriteLine(sum);
+
+            //-----------------------------------------------------
+
+            //While ciklus 
+
+            //int sum = 0;
+
+            //int helper = Convert.ToInt32(Console.ReadLine());
+
+            //while (helper != 0)
+            //{
+            //    sum += helper;
+
+            //    helper = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine(sum);
+            //-----------------------------------------------------
+
+
+            //bool fut = true;
+            //int osszeg = 0;
+
+            //while (fut)
+            //{
+            //    int szam = Convert.ToInt32(Console.ReadLine());
+            //    if (szam == 0)
+            //    {
+            //        fut = false;
+            //    }
+            //    else
+            //    {
+            //        osszeg = osszeg + szam;
+            //    }
+            //}
+
+            //Console.WriteLine(osszeg);
+        }
+    }
+}
