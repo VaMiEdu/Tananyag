3b26814e19ba6d40f159d1b55ba7fecda0bed35a
Mihaly Varga <vmisi20@gmail.com>
2022-11-05 11:35:08 +0100

    Utazasnyilvantarto alkalmazás hozzáadása

---
+++ otodik_ora/Tananyag/LINQ/UtazasNyilvantarto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UtazasNyilvantarto
+{
+    class Utazo
+    {
+        public Dictionary<DateTime, string> Allomasok { get; set; }
+               
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
