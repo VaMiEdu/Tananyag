c20834731856821c44b5b9c0ba6dd7c91424734b
Mihaly Varga <vmisi20@gmail.com>
2022-11-05 15:14:30 +0100

    Struct hozzáadása

---
+++ otodik_ora/Tananyag/Struct/Struct/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Struct
+{
+    public class Passport
+    {
+        public string Number { get; set; }
+        public DateTime Expiration { get; set; }
+        public string Country { get; set; }
+
+        //public int Valami(int elso, int masodik)
+        //{
+        //    return elso + masodik;
+        //}
+    }
+
+    public struct PassportAsStruct
+    {
+        public string Number { get; set; }
+        public DateTime Expiration { get; set; }
+        public string Country { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack & heap (verem & kupac)
+
+            //LIFO & FIFO (last in first out & first in first out)
+
+
+            Passport elsoPassport = new Passport();
+            elsoPassport.Number = "123";
+
+            Passport masodikPassport = elsoPassport;
+            masodikPassport.Number = "456";
+
+            PassportAsStruct passportAsStruct = new PassportAsStruct();
+            passportAsStruct.Number = "12345";
+
+            PassportAsStruct passportAsStruct2 = passportAsStruct;
+            passportAsStruct2.Number = "12";
+
+        }
+    }
+}
