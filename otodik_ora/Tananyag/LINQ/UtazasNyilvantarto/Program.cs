8cc010854cf4543dcaf5fadbd20ee81120f0d94f
Mihaly Varga <vmisi20@gmail.com>
2022-11-05 13:23:46 +0100

    Utazasnyilvantarto új metódusok hozzáadása

--- otodik_ora/Tananyag/LINQ/UtazasNyilvantarto/Program.cs
+++ otodik_ora/Tananyag/LINQ/UtazasNyilvantarto/Program.cs
@@ -6,26 +6,27 @@ namespace UtazasNyilvantarto
 {
     class Utazo
     {
-
         public string Nev { get; set; }
-        public Dictionary<DateTime, string> Allomasok { get; set; }
+        public Dictionary<DateTime, string> Allomasok { get; set; } = new Dictionary<DateTime, string>();
 
         private int latogatottHelyekSzama;
 
-        public int MyProperty
+        public int LatogatottHelyekSzama
         {
-            get { return latogatottHelyekSzama; }
-            set { latogatottHelyekSzama = value; }
+            get
+            {
+                return latogatottHelyekSzama;
+            }
         }
-
-
 
         public Utazo(string nev)
         {
-            //if (string.IsNullOrWhiteSpace(nev))
-            //{
-            //    throw new ArgumentException($"A megadott név ({nev}) nem megfelelő formátumú!");
-            //}
+            //Allomasok = new Dictionary<DateTime, string>();
+
+            if (string.IsNullOrWhiteSpace(nev))
+            {
+                throw new ArgumentException($"A megadott név ({nev}) nem megfelelő formátumú!");
+            }
 
             Nev = nev;
         }
@@ -42,14 +43,54 @@ namespace UtazasNyilvantarto
             return Allomasok.Any(x => x.Value.Equals(helyNeve));
         }
 
-        public int HanyHelyenJart() => latogatottHelyekSzama;
+        public string HolVoltElobb(string egyik, string masik)
+        {
+            var egyikHely = Allomasok.First(x => x.Value == egyik);
+            var masikHely = Allomasok.First(x => x.Value == masik);
+
+            if (egyikHely.Key < masikHely.Key)
+            {
+                return egyikHely.Value;
+            }
+
+            return masikHely.Value;
+
+            //return egyikHely.Key < masikHely.Key ? egyikHely.Value : masikHely.Value;  // conditional operator vagy ternary operator
+
+            //Készítsen metódust, amely megadja azokat a helyeket, ahol az utazó többször is járt!
+            //string[] HelyekAholTobbszorJart()
+
+        }
+
+        public List<string> HelyekAholTobbszorJart()
+        {
+            return Allomasok.GroupBy(x => x.Value).Where(g => g.Count() > 1).Select(g => g.Key).ToList();
+        }
+
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Utazo utazo = new Utazo("Misi");
+
+            utazo.Utazik(new DateTime(2022, 02, 01), "Blahamák");
+            utazo.Utazik(new DateTime(2019, 02, 01), "Hawaii");
+            utazo.Utazik(new DateTime(2000, 02, 01), "Járda szigetek");
+            utazo.Utazik(new DateTime(2000, 12, 01), "Járda szigetek");
+
+            Console.WriteLine(utazo.JartE("Hawaii"));
+
+            Console.WriteLine(utazo.HolVoltElobb("Blahamák", "Járda szigetek"));
+
+            var helyekAholTobbszorJart = utazo.HelyekAholTobbszorJart();
+
+            foreach (var hely in helyekAholTobbszorJart)
+            {
+                Console.WriteLine(hely);
+            }
+
         }
     }
 }
