852c6b2f69f6db517f5d3323a319f3d57bf38204
Mihaly Varga <vmisi20@gmail.com>
2022-11-12 13:32:35 +0100

    Auto hozzáadása

--- hatodik_ora/Tananyag/Bicikli/Bicikli/Program.cs
+++ hatodik_ora/Tananyag/Bicikli/Bicikli/Program.cs
@@ -1,57 +1,187 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bicikli
 {
-    class Bicikli
+    class Jarmu
     {
-        private bool megy;
+        private bool megyE;
+        private double sebesseg;
+
+        public List<Kerek> Kerekek = new List<Kerek>();
 
         public Lampa ElsoLampa { get; set; }
         public Lampa HatsoLampa { get; set; }
 
-        private Pedal pedal = new Pedal();
+        public double Sebesseg { get => sebesseg; set => sebesseg = value; }
 
-        public Pedal Pedal
+        public void Lassit(double mennyiseg)
         {
-            get 
+            if (megyE)
             {
-                return pedal; 
+                if (mennyiseg > Sebesseg)
+                {
+                    Sebesseg = 0;
+                    megyE = false;
+                }
+                else
+                {
+                    Sebesseg -= mennyiseg;
+                }
             }
-            set 
+
+        }
+
+        public void Gyorsit(double mennyiseg)
+        {
+            if (KerekekNyomasaOk() && LampakMukodnekE())
             {
-                pedal = value;
+                Sebesseg += mennyiseg;
+                megyE = true;
             }
         }
 
-        public void Elindul()
+        private bool KerekekNyomasaOk()
         {
-            Pedal.HajtvaVan = true;
-            megy = true;
+            //bool vanEOlyanKerekAminekKisebbANyomasaMint6Bar = false;
+
+            //foreach (var kerek in Kerekek)
+            //{
+            //    if (kerek.Nyomas < 6)
+            //    {
+            //        vanEOlyanKerekAminekKisebbANyomasaMint6Bar = true;
+            //    }
+            //}
+
+            //return vanEOlyanKerekAminekKisebbANyomasaMint6Bar;
+
+            return Kerekek.All(x => x.Nyomas >= 6);
         }
 
+        private bool LampakMukodnekE()
+        {
+            var elsoLampaMukodik = ElsoLampa.Mukodik;
+            var hatsoLampaMukodik = HatsoLampa.Mukodik;
 
+            return elsoLampaMukodik && hatsoLampaMukodik;
+        }
+    }
+
+    class Auto : Jarmu
+    {
+        public Motor Motor { get; set; }
+
+        public Auto()
+        {
+        }
+
+        public new void Gyorsit(double mennyiseg)
+        {
+            if (mennyiseg + Sebesseg <= 120)
+            {
+                base.Gyorsit(mennyiseg);
+            }
+        }
+    }
+
+    class Bicikli : Jarmu
+    {
+        public Pedal Pedal { get; set; }
+
+        public new void Gyorsit(double mennyiseg)
+        {
+            if (mennyiseg + Sebesseg <= 40)
+            {
+                base.Gyorsit(mennyiseg);
+            }
+        }
+    }
+
+
+    class Kerek
+    {
+        public double Nyomas { get; set; }
     }
 
     class Lampa
     {
-
+        public bool Mukodik { get; set; }
     }
 
     class Pedal
     {
-        public bool HajtvaVan { get; set; }
+
+    }
+
+    class Motor
+    {
+
+    }
+
+    class Pumpa
+    {
+        public void Felfuj(Kerek kerek)
+        {
+            kerek.Nyomas += 1;
+        }
+
+        public void Felfuj(List<Kerek> kerekek, double idealisNyomas)
+        {
+            foreach (var kerek in kerekek)
+            {
+                if (kerek.Nyomas < idealisNyomas)
+                {
+                    kerek.Nyomas = idealisNyomas;
+                }
+            }
+        }
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Bicikli b = new Bicikli();
+            Auto kocsi = new Auto();
 
-            b.Pedal.HajtvaVan = true;
+            Lampa elsoLampa = new Lampa();
+            Lampa hatsoLampa = new Lampa();
 
-            b.Elindul();
-            Console.WriteLine("Hello World!");
+            elsoLampa.Mukodik = true;
+            hatsoLampa.Mukodik = true;
+
+            Kerek kerek1 = new Kerek();
+            Kerek kerek2 = new Kerek();
+            Kerek kerek3 = new Kerek();
+            Kerek kerek4 = new Kerek();
+
+            List<Kerek> kerekek = new List<Kerek>()
+            {
+                kerek1, kerek2, kerek3, kerek4
+            };
+
+            Pumpa pumpa = new Pumpa();
+            pumpa.Felfuj(kerek1);
+
+            pumpa.Felfuj(kerekek, 6);
+
+
+
+            kerek1.Nyomas = 8;
+            kerek2.Nyomas = 8;
+            kerek3.Nyomas = 8;
+            kerek4.Nyomas = 8;
+
+            kocsi.HatsoLampa = hatsoLampa;
+            kocsi.ElsoLampa = elsoLampa;
+            kocsi.Kerekek.Add(kerek1);
+            kocsi.Kerekek.Add(kerek2);
+            kocsi.Kerekek.Add(kerek3);
+            kocsi.Kerekek.Add(kerek4);
+
+            kocsi.Gyorsit(10.0);
+
+
         }
     }
 }
