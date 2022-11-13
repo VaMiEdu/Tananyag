981d44c893ab027ce5a5bca25307b782c4ae5d5d
Mihaly Varga <vmisi20@gmail.com>
2022-11-13 15:05:57 +0100

    Konyha alkalmazás hozzáadása

---
+++ hatodik_ora/Tananyag/RobotGep/RobotGep/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RobotGep
+{
+    class RobotGep
+    {
+        protected bool MukodikE;
+
+        public void Leallit()
+        {
+            MukodikE = false;
+        }
+
+        public virtual void Mukodik()
+        {
+            Console.WriteLine("Megy a robotgép!");
+            MukodikE = true;
+        }
+    }
+
+    class TurmixGep : RobotGep
+    {
+        public new void Mukodik()
+        {
+            MukodikE = true;
+
+            Console.WriteLine("Megy a turmixgép!");
+        }
+    }
+
+    class KaveDaralo : RobotGep
+    {
+        public new void Mukodik()
+        {
+            MukodikE = true;
+
+            Console.WriteLine("Megy a kavédaráló!");
+        }
+    }
+
+    class Huto
+    {
+        public List<IElelmiszer> Elelmiszerek { get; set; }
+    }
+
+    interface IElelmiszer
+    {
+        DateTime LejaratiIdo { get; set; }
+        bool FogyaszthatoE { get; }
+        int Mennyiseg { get; set; }
+    }
+
+    class Kave : IElelmiszer
+    {
+        public DateTime LejaratiIdo { get; set; }
+
+        private bool fogyaszthatoE;
+
+        public bool FogyaszthatoE
+        {
+            get 
+            {
+                //TODO implement logic;
+                return fogyaszthatoE; 
+            }
+        }
+
+        public int Mennyiseg { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+    class Gyumolcs : IElelmiszer
+    {
+        public DateTime LejaratiIdo { get; set; }
+
+        private bool fogyaszthatoE;
+
+        public bool FogyaszthatoE
+        {
+            get
+            {
+                //TODO implement logic;
+                return fogyaszthatoE;
+            }
+        }
+
+        public int Mennyiseg { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RobotGep robotGep = new RobotGep();
+            TurmixGep turmixGep = new TurmixGep();
+            KaveDaralo kvDaralo = new KaveDaralo();
+
+            robotGep.Mukodik();
+            turmixGep.Mukodik();
+            kvDaralo.Mukodik();
+        }
+    }
+}
