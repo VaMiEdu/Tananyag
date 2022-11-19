0c94e14101e32f80dcb43d11d20a0ea901f04c26
Mihaly Varga <vmisi20@gmail.com>
2022-11-19 13:19:01 +0100

    Generikusok példa

---
+++ hetedik_ora/Tananyag/Tananyag/Generikusok/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Generikusok
+{
+    class Ellenorzes<T>
+    {
+        //public bool Egyenloek(int elso, int masodik)
+        //{
+        //    return elso == masodik;
+        //}
+
+        //public bool Egyenloek(double elso, double masodik)
+        //{
+        //    return elso == masodik;
+        //}
+
+        //public bool Egyenloek(string elso, string masodik)
+        //{
+        //    return elso == masodik;
+        //}
+
+        public bool Egyenloek<T>(T elso, T masodik)
+        {
+            return elso.Equals(masodik);
+            //return elso == masodik;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ellenorzes<int> ellenorzes = new Ellenorzes<int>();
+
+            //ellenorzes.Egyenloek(3, 4);
+
+            ellenorzes.Egyenloek<int>(3, 4);
+
+            Ellenorzes<string> stringEllenorzes = new Ellenorzes<string>();
+            stringEllenorzes.Egyenloek<string>("elso", "masodik");
+        }
+    }
+}
