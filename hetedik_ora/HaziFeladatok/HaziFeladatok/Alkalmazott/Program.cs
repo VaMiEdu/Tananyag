8daa67d080ea4d7b743d1b980cc1b14045dad11e
Mihaly Varga <vmisi20@gmail.com>
2022-11-19 11:23:24 +0100

    Hatodik órai házifeladat

---
+++ hetedik_ora/HaziFeladatok/HaziFeladatok/Alkalmazott/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Alkalmazott
+{
+
+    //    - Hozzunk létre egy Alkalmazott abstract class-t melynek legyen Nev, Eletkor property-je.
+    //- Hozzuk létre a FixAlkalmazott és Contractor class-okat, melyek az Alkalmazottt class-ból származzanak.
+
+    //- A FixAlkalmazott class-nak legyen egy Fizetes full property-je, melynek értékét a constructoron keresztül állítsuk be.
+    //- A
+
+    //Contractor   class-nak legyen továbbá egy Oraber full property-je, melynek értékét a constructoron keresztül állítsuk be.
+    //- Overrideoljuk a származtatott classokban a ToString() metódust, melyen keresztül írassuk ki a class adatait. (név, életkor, fizetés)
+
+    abstract class Alkalmazott
+    {
+        public string Nev { get; set; }
+        public int Eletkor { get; set; }
+
+        public override string ToString()
+        {
+            return $"Név: {Nev} \n Életkor: {Eletkor} \n".ToString();
+        }
+    }
+
+    class FixAlkalmazott : Alkalmazott
+    {
+        private int fizetes;
+
+        public int Fizetes
+        {
+            get { return fizetes; }
+            set { fizetes = value; }
+        }
+
+        public FixAlkalmazott(int fizetes)
+        {
+            this.fizetes = fizetes;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} Fizetés: {Fizetes} \n " ;
+        }
+    }
+
+    class Contractor : Alkalmazott
+    {
+        private int oraber;
+
+        public int Oraber
+        {
+            get { return oraber; }
+            set { oraber = value; }
+        }
+
+        public Contractor(int oraber)
+        {
+            Oraber = oraber;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} Órabér: {Oraber} \n ";
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ hetedik_ora/HaziFeladatok/HaziFeladatok/RobotGep/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RobotGep
+{
+    internal class Program
+    {
+        class RobotGep
+        {
+            protected bool MukodikE;
+
+            public void Leallit()
+            {
+                MukodikE = false;
+            }
+
+            public virtual void Mukodik()
+            {
+                Console.WriteLine("Megy a robotgép!");
+                MukodikE = true;
+            }
+        }
+
+        class TurmixGep : RobotGep
+        {
+            public new void Mukodik()
+            {
+                MukodikE = true;
+
+                Console.WriteLine("Megy a turmixgép!");
+            }
+
+            public void Turmixol(List<Gyumolcs> gyumolcsok)
+            {
+                foreach (var gyumolcs in gyumolcsok)
+                {
+                    if (gyumolcs.Mennyiseg > 1)
+                    {
+                        gyumolcs.Mennyiseg -= 1;
+                    }
+                }
+            }
+        }
+
+        class KaveDaralo : RobotGep
+        {
+            public new void Mukodik()
+            {
+                MukodikE = true;
+
+                Console.WriteLine("Megy a kavédaráló!");
+            }
+
+            public void KavetDaral(Kave kave)
+            {
+                if (kave.Mennyiseg < 1)
+                {
+                    return;
+                }
+
+                kave.Mennyiseg -= 1;
+            }
+        }
+
+        class Huto
+        {
+            public List<IElelmiszer> Elelmiszerek { get; set; }
+        }
+
+        interface IElelmiszer
+        {
+            DateTime LejaratiIdo { get; set; }
+            bool FogyaszthatoE { get; }
+            int Mennyiseg { get; set; }
+        }
+
+        class Kave : IElelmiszer
+        {
+            public DateTime LejaratiIdo { get; set; }
+
+            private int mennyiseg;
+
+            public bool FogyaszthatoE
+            {
+                get
+                {
+                    return DateTime.Now < LejaratiIdo;
+                }
+            }
+
+            public int Mennyiseg
+            {
+                get
+                {
+                    return mennyiseg;
+                }
+
+                set
+                {
+                    if (value > 0)
+                    {
+                        mennyiseg = value;
+                    }
+                }
+            }
+
+            public Kave(DateTime lejaratiIdo, int mennyiseg)
+            {
+                LejaratiIdo = lejaratiIdo;
+                Mennyiseg = mennyiseg;
+            }
+
+
+        }
+
+        class Gyumolcs : IElelmiszer
+        {
+            public DateTime LejaratiIdo { get; set; }
+
+            private bool fogyaszthatoE;
+
+            public bool FogyaszthatoE
+            {
+                get
+                {
+                    //TODO implement logic;
+                    return fogyaszthatoE;
+                }
+            }
+
+            public int Mennyiseg { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        }
+
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                RobotGep robotGep = new RobotGep();
+                TurmixGep turmixGep = new TurmixGep();
+                KaveDaralo kvDaralo = new KaveDaralo();
+
+                robotGep.Mukodik();
+                turmixGep.Mukodik();
+                kvDaralo.Mukodik();
+            }
+        }
+    }
+}
