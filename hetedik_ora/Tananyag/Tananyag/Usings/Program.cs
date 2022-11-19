f264fc27ab9d6c18547016e8e429dd4a7959c433
Mihaly Varga <vmisi20@gmail.com>
2022-11-19 11:24:17 +0100

    Usings példa

---
+++ hetedik_ora/Tananyag/Tananyag/Usings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Usings;
+
+namespace Usings
+{
+    class Kutya : IDisposable
+    {
+        public void Dispose()
+        {
+
+        }
+        //public void Dispose()
+        //{
+        //    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //    Dispose(disposing: true);
+        //    GC.SuppressFinalize(this);
+        //}
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string szoveg = "Do not change this code. Put cleanup code in 'Dispose(bool disposing)' meth";
+
+        StringReader reader = new StringReader(szoveg);
+
+        using (var stringReader = new StringReader(szoveg))
+        {
+            Console.WriteLine(stringReader.ReadLine());
+        }
+
+        Console.WriteLine("Hello World!");
+    }
+}
