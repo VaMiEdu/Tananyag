7a40a3f13f8f67a51146018c3d0136a897e1f4d1
Mihaly Varga <vmisi20@gmail.com>
2022-11-26 10:30:24 +0100

    Intro wpf alkalmazás

---
+++ nyolcadik_ora/tananyag/wpf/wpf/Intro/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Intro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //TextBlock tb = new TextBlock();
+            //tb.TextWrapping = TextWrapping.Wrap;
+
+            //tb.Inlines.Add("valamilyen szoveg ");
+            //tb.Inlines.Add(new Run("valamilyen szoveg") { FontWeight = FontWeights.Bold });
+
+            //this.Content = tb;
+
+        }
+
+    }
+}
